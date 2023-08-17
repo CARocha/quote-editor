509d0af3f8eb2c276773ad228ceb2f32c40da31b
Carlos Rocha <crocha09.09@gmail.com>
2023-08-16 21:11:30 -0600

    adds stimulus for flash messages remove for the dom

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
+# helper for flash messages
 module ApplicationHelper
+  def render_turbo_stream_flash_messages
+    turbo_stream.prepend 'flash', partial: 'layouts/flash'
+  end
 end
